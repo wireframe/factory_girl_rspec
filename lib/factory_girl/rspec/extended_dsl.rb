a709a5a817d23ada88a958e9e3eda4a147496ceb
Chris Hendrix <chris.hendrix@workday.com>
2012-09-16 15:42:35 -0700

    changed back to default create, with option for build instead

--- lib/factory_girl/rspec/extended_dsl.rb
+++ lib/factory_girl/rspec/extended_dsl.rb
@@ -4,11 +4,11 @@ module FactoryGirl
   module Rspec
     module ExtendedDsl
       def with(name, options = {}, &block)
-        subject(name) { ::FactoryGirl.build(name, options, &block) }
+        subject(name) { ::FactoryGirl.create(name, options, &block) }
       end
       
-      def create(name, options = {}, &block)
-        subject(name) { ::FactoryGirl.create(name, options, &block) }
+      def build(name, options = {}, &block)
+        subject(name) { ::FactoryGirl.build(name, options, &block) }
       end 
     end
   end
